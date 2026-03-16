f85b3427f6317dbc41ba438e1163a5a84a31725b
tanyasingh3102006-cpu <YOUR_EMAIL_USED_IN_THIS_GITHUB_ACCOUNT>
2026-03-16 09:56:59 +0530

    ignore file added

--- client/src/routes/RootRouter.jsx
+++ client/src/routes/RootRouter.jsx
@@ -1,39 +1,42 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { allRoutes } from './routes';
 
 /**
  * RootRouter Component
- * 
- * Uses standard declarative JSX structure as requested.
+ * Handles all application routes
  */
+
 const RootRouter = () => {
     return (
-        <Routes>
-            {allRoutes.map((route, index) => {
-                if (route.children) {
+        <BrowserRouter>
+            <Routes>
+                {allRoutes.map((route, index) => {
+                    if (route.children) {
+                        return (
+                            <Route key={index} path={route.path} element={route.element}>
+                                {route.children.map((child, childIdx) => (
+                                    <Route
+                                        key={childIdx}
+                                        index={child.index}
+                                        path={child.path}
+                                        element={child.element}
+                                    />
+                                ))}
+                            </Route>
+                        );
+                    }
+
                     return (
-                        <Route key={index} path={route.path} element={route.element}>
-                            {route.children.map((child, childIdx) => (
-                                <Route
-                                    key={childIdx}
-                                    index={child.index}
-                                    path={child.path}
-                                    element={child.element}
-                                />
-                            ))}
-                        </Route>
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={route.element}
+                        />
                     );
-                }
-                return (
-                    <Route
-                        key={index}
-                        path={route.path}
-                        element={route.element}
-                    />
-                );
-            })}
-        </Routes>
+                })}
+            </Routes>
+        </BrowserRouter>
     );
 };
 
